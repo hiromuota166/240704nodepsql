86b28e0c08451d0e6d4247575a4cdde49381d4b2
hiromuota166 <otahiromu166@gmail.com>
2024-07-08 00:04:11 +0900

    add cors

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const cors = require('cors');
 const app = express();
 const port = 8080;
 
+app.use(cors());
+
 // JSONを返すGETエンドポイント
-app.get('/api', cors(), (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
