f2111bb0f1135b591ace7c595b262f243b0e10df
hiromuota166 <otahiromu166@gmail.com>
2024-07-07 23:48:43 +0900

    add second getMethod

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/api', cors(), (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
+app.get('/api/notcors', (req, res) => {
+  res.json({ message: 'Hello, World! here message is not cors' });
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
