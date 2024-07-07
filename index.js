5e533b53f76cb4945446449ee73d3b83ac1df5bd
hiromuota166 <otahiromu166@gmail.com>
2024-07-08 01:08:39 +0900

    add post

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
 app.use(cors());
+
+app.use(bodyParser.json());
 
 // JSONを返すGETエンドポイント
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
-app.get('/api/notcors', (req, res) => {
-  res.json({ message: 'Hello, World! here message is not cors' });
+// JSONを返すPOSTエンドポイント
+app.post('/api', (req, res) => {
+  const value = req.body.value; //expressの書き方
+
+  if (value === 'a') {
+    res.json({ message: 'value is a' });
+  } else if (value === 'b') {
+    res.json({ message: 'value is b' });
+  } else {
+    res.json({ message: 'value is neither a nor b' });
+  }
 });
 
 app.listen(port, () => {
