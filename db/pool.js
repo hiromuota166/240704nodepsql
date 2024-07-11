4687c8aa46c19dad22e91f3a58743ffaa1e6492c
hiromuota166 <otahiromu166@gmail.com>
2024-07-11 13:40:59 +0900

    add postgresqlサーバーをPCからrenderに変更

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'ootakeiyume',
-  host: 'two40704nodepsql.onrender.com',
-  database: 'linedb',
-  password: 'hiromu166',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
